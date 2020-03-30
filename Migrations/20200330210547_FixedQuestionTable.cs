f9c7b7d20798606caeb4816014ef1c069d375a9f
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-30 17:06:31 -0400

    fixed question table

---
+++ Migrations/20200330210547_FixedQuestionTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuncoastOverflow.Migrations
+{
+    public partial class FixedQuestionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Asked",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Questions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Asked",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Questions");
+        }
+    }
+}
--- Models/Question.cs
+++ Models/Question.cs
@@ -7,6 +7,8 @@ namespace SuncoastOverflow.Models
   {
     public int Id { get; set; }
     public string Author { get; set; }
+    public string Title { get; set; }
+    public string Asked { get; set; }
     public int VoteCount { get; set; }
     public bool Resolved { get; set; }
     public DateTime TimeSubmitted { get; set; }
