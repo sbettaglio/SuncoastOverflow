e274288be38adbdecf5fbbf5db15828a1b95728a
Daniel D Demerin <ddemerin@gmail.com>
2020-03-30 17:36:29 -0400

    updating dan-branch

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Home() {
   return (
     <div>
-      <h1>This is where we start!</h1>
+      <h1>This is where we start! This is where we get confused!</h1>
     </div>
   )
 }
