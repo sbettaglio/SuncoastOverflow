31c2cfe60b213b5183f96c2043012bbf9fdcbece
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-31 17:15:14 -0400

    added footer to ask question

--- ClientApp/src/pages/AskQuestion.jsx
+++ ClientApp/src/pages/AskQuestion.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import QuestionDropdown from '../components/QuestionDropdown'
+import { Link } from 'react-router-dom'
 const AskQuestion = () => {
   return (
     <>
@@ -43,6 +44,78 @@ const AskQuestion = () => {
           <button className="submit-question">Post your question</button>
         </form>
       </main>
+      <footer className="footerLinks">
+        {/* <ul> */}
+        {/* <li> */}
+        <section className="linksLeft">
+          <ul>
+            <li>
+              <Link to="/">Questions</Link>
+            </li>
+            <li>
+              <Link to="/">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/">Developer Jobs</Link>
+            </li>
+            <li>
+              <Link to="/">Salary Calculator</Link>
+            </li>
+            <li>
+              <Link to="/">Help</Link>
+            </li>
+            <li>
+              <Link to="/">Mobile</Link>
+            </li>
+            <li>
+              <Link to="/">Disable Responsiveness</Link>
+            </li>
+          </ul>
+        </section>
+        {/* </li> */}
+        {/* <li> */}
+        <section className="linksCenter">
+          <ul>
+            <li>
+              <Link to="/question">Teams</Link>
+            </li>
+            <li>
+              <Link to="/">Talent</Link>
+            </li>
+            <li>
+              <Link to="/">Advertising</Link>
+            </li>
+            <li>
+              <Link to="/">Enterprise</Link>
+            </li>
+          </ul>
+        </section>
+        {/* </li> */}
+        {/* </ul> */}
+        <section className="linksRight">
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Questions</Link>
+            </li>
+            <li>
+              <Link to="/">Work Here</Link>
+            </li>
+            <li>
+              {' '}
+              <Link to="/">Legal</Link>
+            </li>
+            <li>
+              <Link to="/">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/">Contact Us</Link>
+            </li>
+          </ul>
+        </section>
+      </footer>
     </>
   )
 }
