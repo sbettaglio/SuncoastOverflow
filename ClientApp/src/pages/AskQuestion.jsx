1e6c7cc9f85558664a76fe8b207f21875839eda1
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-31 15:16:59 -0400

    ask question dropdown menu

--- ClientApp/src/pages/AskQuestion.jsx
+++ ClientApp/src/pages/AskQuestion.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
 const AskQuestion = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggle = () => setDropdownOpen(prevState => !prevState)
   return (
     <>
       <h1>Ask a public question</h1>
-      <select className="question-dropdown">
-        <option>Step 1: Draft your question</option>
-        <option>1. Summarize the problem</option>
-        <option>2. Describe what you've tried</option>
-        <option>3. Show some code</option>
-      </select>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>Step 1: Draft your question</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>
+            The Community is here to hel you with specific coding, algorithm, or
+            language problems.
+          </DropdownItem>
+          <DropdownItem>1. Summarize the problem</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>3. Describe what you've tried</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Show some code</DropdownItem>
+          <DropdownItem divider />
+        </DropdownMenu>
+      </Dropdown>
     </>
   )
 }
